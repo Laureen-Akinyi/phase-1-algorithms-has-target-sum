5738d0ad8ae6d45ff1c7fd1ae35fa99ff0e31e9d
laureenakinyi <laureen.akinyi@student.moringaschool.com>
2022-06-20 15:45:57 +0300

    commit tree c68a59d0245c3163c802a3073cb87fbc4e2f618f

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function hasTargetSum(array, target) {
+  const seenNumbers = {};
+  for (const number of array){
+    const complement = target - number;
+
+    if(complement in seenNumbers)
+     return true
+     
+  }
+  return false
   // Write your algorithm here
 }
 
 /* 
   Write the Big O time complexity of your function here
+  space complexity = 0(1)
+  time complexity = 0(n^2)
 */
 
 /* 
   Add your pseudocode here
+  create a function hasTargetSum that takes in two argument that is an array integer and a target integer
+  The function should check if two numbers add up to the target integer
+  If it adds up to the target the return value should be true
+  Else return false
 */
 
 /*
@@ -27,8 +42,30 @@ if (require.main === module) {
 
   console.log("");
 
+
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4,8],3));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4],5));
+
+  console.log("")
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 2, 3, 3],4));
+
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4],4));
 }
 
 module.exports = hasTargetSum;
